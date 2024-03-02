ac2d767650ca5b5c77f7e1d9876cbcab5dc13c76
ALVAROVELAME <136656117+ALVAROVELAME@users.noreply.github.com>
2024-03-02 19:49:18 -0300

    nova pagina

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 function Home() {
-    return <div>Home</div>
+    return <>
+    <div>Home</div>
+    <div>
+        <p>Alvaro Velame</p>
+        <p>Ola</p>
+    </div>
+    
+    </>
 }
 
 export default Home
