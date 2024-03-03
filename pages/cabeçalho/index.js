a2e975fbef2e0f1e2e4c1c5fe4297f074f73975a
ALVAROVELAME <136656117+ALVAROVELAME@users.noreply.github.com>
2024-03-02 23:00:23 -0300

    novoComit

--- pages/cabeçalho/index.js
+++ pages/cabeçalho/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import styles from './cabecalho.module.css';
 
 function Cabecalho() {
   return (
-    <header>
-      <h1>Meu Site</h1>
+    <header className={styles.container}>
+      <h1 id={styles.titulo}>Alvaro Velame</h1>
       <nav>
-        <ul>
+        <ul className={styles.menus}>
           <li><a href="#">Início</a></li>
           <li><a href="#">Sobre</a></li>
           <li><a href="#">Contato</a></li>
+          <li><a href="#">Deixe um Orçamento</a></li>
         </ul>
       </nav>
     </header>
