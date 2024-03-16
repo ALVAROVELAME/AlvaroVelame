1187b68c140452d1a930107acc9dc6f2ae7faec8
ALVAROVELAME <136656117+ALVAROVELAME@users.noreply.github.com>
2024-03-15 21:58:33 -0300

    consertando erros

--- pages/conteudo Principal/index.js
+++ pages/conteudo Principal/index.js
@@ -3,51 +3,26 @@ import styles from './index.module.css'; // Importa o módulo CSS
 
 export default function ConteudoPrincipal() {
   return (
-    <>
-      <div className={styles.conteudoPrincipal}>
-        <div>
-          <h2>Sobre Mim</h2>
-          <p>
-            Olá! Sou Álvaro Velame, um entusiasta do desenvolvimento de software com experiência em Java, JavaScript e Python. Além disso, tenho expertise em automações e scraping, o que me permite simplificar processos e extrair informações valiosas da web.
-          </p>
-        </div>
-        <div className={styles.imagem}>
-          <img src="https://media.giphy.com/media/26tn33aiTi1jkl6H6/giphy.gif" alt="Imagem" />
-        </div>
-
-        <div>
-          <h2>Serviços Oferecidos</h2>
-          <p>
-            <ul>
-              <li>Desenvolvimento de Aplicações Web e Desktop em Java, JavaScript e Python</li>
-              <li>Automações e Scraping para otimização de processos e coleta de dados na web</li>
-              <li>Consultoria em Desenvolvimento de Software</li>
-            </ul>
-          </p>
-        </div>
-        <div>
-          <div className={styles.imagem}>
-            <img src="https://media.giphy.com/media/26tn33aiTi1jkl6H6/giphy.gif" alt="Imagem" />
-          </div>
-        </div>
-        <div>
-          <h2>Tecnologias</h2>
-          <h6>Minhas habilidades abrangem:</h6>
-          <p>
-            <ul>
-              <li>Linguagens de Programação: Java, JavaScript, Python</li>
-              <li>Frameworks e Bibliotecas: Spring Boot, React.js, Node.js, Flask</li>
-              <li>Automação e Scraping: Selenium, BeautifulSoup, Puppeteer</li>
-              <li>Banco de Dados: SQL (MySQL, PostgreSQL), NoSQL (MongoDB)</li>
-            </ul>
-          </p>
-        </div>
-        <div>
-          <div className={styles.imagem}>
-            <img src="https://media.giphy.com/media/26tn33aiTi1jkl6H6/giphy.gif" alt="Imagem" />
-          </div>
-        </div>
+    <div className={styles.container}>
+      <div className={styles.column}>
+        <h2>Sobre Mim</h2>
+        <p>
+          Olá! Sou Álvaro Velame, um entusiasta do desenvolvimento de software com experiência em Java, JavaScript e Python. Além disso, tenho expertise em automações e scraping, o que me permite simplificar processos e extrair informações valiosas da web.
+        </p>
+        <a href="https://www.seu-link.com">veja o Projeto</a>
+        <img src="https://media.giphy.com/media/26tn33aiTi1jkl6H6/giphy.gif" alt="Imagem" />
       </div>
-    </>
+
+      <div className={styles.column}>
+        <h2>Serviços Oferecidos</h2>
+        <ul>
+          <li>Desenvolvimento de Aplicações Web e Desktop em Java, JavaScript e Python</li>
+          <li>Automações e Scraping para otimização de processos e coleta de dados na web</li>
+          <li>Consultoria em Desenvolvimento de Software</li>
+        </ul>
+        <a href="https://www.seu-link.com">Veja o projeto</a>
+        <img src="https://media.giphy.com/media/26tn33aiTi1jkl6H6/giphy.gif" alt="Imagem" />
+      </div>
+    </div>
   );
 };
--- pages/rodape/index.js
+++ pages/rodape/index.js
@@ -5,7 +5,7 @@ import styles from './index.module.css';
 export default function Rodape() {
   return (
     <footer className={styles.rodape}>
-      <p>&copy; {new Date().getFullYear()} Meu Site. Todos os direitos reservados.</p>
+      <p>&copy; {new Date().getFullYear()} Alvaro Velame. Todos os direitos reservados.</p>
     </footer>
   );
 }
